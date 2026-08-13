d901d69ec8553ca4080fdf38f7a9b870d7e2cf9c
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-08-13 20:58:29 +0300

    pep8 adjustments

--- property/admin.py
+++ property/admin.py
@@ -13,7 +13,14 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'flat_owners__owner',)
     readonly_fields = ('created_at',)
     inlines = [OwnerInline]
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'get_owners',)
+    list_display = (
+        'address',
+        'price',
+        'new_building',
+        'construction_year',
+        'town',
+        'get_owners',
+    )
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
     raw_id_fields = ('liked_by',)
